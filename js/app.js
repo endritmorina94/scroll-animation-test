b67ef6e3716d229e0ab824991890661526707090
endritmorina94 <endrit.morina@outlook.it>
2021-05-19 00:56:25 +0200

    GSAP implementation

--- js/app.js
+++ js/app.js
@@ -1,13 +1,37 @@
+var tl = gsap.timeline({repeat: -1, repeatDelay: 1});
 
-var app = new Vue(
-    {
-        el: '#root',
-        data: {
-        },
-        methods: {
-        },
-        mounted() {
-            
-        }
-    }
-);
+    tl.to(".phone-content", {
+        opacity: 1,
+        scale: 1,
+        duration: 1
+    });
+
+    tl.to(".btn", {
+        scale: .9,
+        color: "black",
+        backgroundColor: "white",
+        duration: .2,
+        delay: 1
+    });
+
+    tl.to(".btn", {
+        scale: 1,
+        duration: .5,
+        delay: .2
+    });
+
+    tl.to(".prices", {
+        scale: 1,
+        opacity: 1,
+        duration: .5
+    });
+
+    tl.to(".unlock", {
+        y: 100,
+        opacity: 0,
+        duration: .7,
+        delay: 2
+    });
+
+    tl.to(".phone-content", {opacity: 1, duration: 6.5});
+    tl.to(".phone-content", {opacity: 0, duration: .5});
