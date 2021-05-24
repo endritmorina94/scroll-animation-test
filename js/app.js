a6b7bab5792534724c0f67860241fcde239a05a7
endritmorina94 <endrit.morina@outlook.it>
2021-05-24 16:09:17 +0200

    first phone ended

--- js/app.js
+++ js/app.js
@@ -1,6 +1,143 @@
 var checkmarkThirdPhone = CSSRulePlugin.getRule(".checkmark:after");
 var checkmark = CSSRulePlugin.getRule(".two .checkmark:after");
 
+
+var firstPhone = gsap.timeline({repeat: -1, repeatDelay: 2});
+
+    //STARTING ANIMATION START
+    firstPhone.to(".one .phone-content", {
+        padding: "60px 25px 30px",
+        opacity: 1,
+        duration: 1
+    });
+    //STARTING ANIMATION END
+
+    //Pulse Effect
+    firstPhone.from(".plus", {
+        boxShadow: "0 0 0 0 rgba(255, 255, 255, 0.7), 0 0 0 0 rgba(255, 255, 255, 0.7)",
+        duration: 0.6
+    });
+    firstPhone.to(".plus", {
+        boxShadow: "0 0 0 20px rgba(255, 255, 255, 0), 0 0 0 0 rgba(255, 255, 255, 0.7)",
+        duration: 0.4
+    });
+    firstPhone.to(".plus", {
+        boxShadow: "0 0 0 20px rgba(255, 255, 255, 0), 0 0 0 15px rgba(255, 255, 255, 0)",
+        duration: 0.4
+    });
+    firstPhone.to(".plus", {
+        backgroundColor: "white",
+        duration: 0.2,
+        delay: -0.5
+    });
+    firstPhone.to(".plus", {
+        backgroundColor: "rgb(150, 150, 150)",
+        duration: 0
+    });
+    firstPhone.to(".plus", {
+        boxShadow: "0 0 0 0 rgba(255, 255, 255, 0), 0 0 0 15px rgba(255, 255, 255, 0)",
+        duration: 0.4
+    });
+    //Pulse Effect End
+
+    firstPhone.from(".displayed", {
+        top: "148px",
+        ease: Power3.easeOut,
+        duration: 0.8,
+        delay: -0.8
+    });
+    firstPhone.to(".displayed", {
+        top: "222px",
+        ease: Power3.easeOut,
+        duration: 0.6
+    });
+    firstPhone.to(".usd", {
+        opacity: 1,
+        duration: 0.8,
+        delay: -0.6
+    });
+    firstPhone.to(".amounts", {
+        top: "-36px",
+        ease: Power3.easeOut,
+        duration: 0.5,
+        delay: -0.8
+    });
+    firstPhone.to(".usd", {
+        scale: 1,
+        duration: 0.3,
+        delay: -0.7
+    });
+
+    //Pulse Effect
+    firstPhone.from(".plus", {
+        boxShadow: "0 0 0 0 rgba(255, 255, 255, 0.7), 0 0 0 0 rgba(255, 255, 255, 0.7)",
+        duration: 0.6
+    });
+    firstPhone.to(".plus", {
+        boxShadow: "0 0 0 20px rgba(255, 255, 255, 0), 0 0 0 15px rgba(255, 255, 255, 0)",
+        duration: 0.4
+    });
+    firstPhone.to(".plus", {
+        backgroundColor: "white",
+        duration: 0.2,
+        delay: -0.5
+    });
+    firstPhone.to(".plus", {
+        backgroundColor: "rgb(150, 150, 150)",
+        duration: 0
+    });
+    firstPhone.to(".plus", {
+        boxShadow: "0 0 0 0 rgba(255, 255, 255, 0), 0 0 0 15px rgba(255, 255, 255, 0)",
+        duration: 0.4
+    });
+
+    firstPhone.to(".plus", {
+        boxShadow: "0 0 0 0 rgba(255, 255, 255, 0.7), 0 0 0 0 rgba(255, 255, 255, 0.7)",
+        duration: 0
+    });
+    //Pulse Effect End
+
+    firstPhone.to(".usd", {
+        top: "222px",
+        ease: Power3.easeOut,
+        duration: 0.6,
+        delay: -0.6
+    });
+    firstPhone.to(".displayed", {
+        top: "296px",
+        ease: Power3.easeOut,
+        duration: 0.6,
+        delay: -0.6
+    });
+    firstPhone.to(".naira", {
+        opacity: 1,
+        duration: 0.8,
+        delay: -0.6
+    });
+    firstPhone.to(".amounts", {
+        top: 0,
+        ease: Power3.easeOut,
+        duration: 0.5,
+        delay: -0.8
+    });
+    firstPhone.to(".naira", {
+        scale: 1,
+        duration: 0.3,
+        delay: -0.7
+    });
+
+    //CLOSING ANIMATION START
+    firstPhone.to(".one .phone-content", {
+        opacity: 1,
+        duration: 3
+    });
+
+    firstPhone.to(".one .phone-content", {
+        opacity: 0,
+        duration: 1
+    });
+    //CLOSING ANIMATION END
+
 //SECOND PHONE ANIMATION START
 var secondPhone = gsap.timeline({repeat: -1, repeatDelay: 1});
     //STARTING ANIMATION START
@@ -39,8 +176,8 @@ var secondPhone = gsap.timeline({repeat: -1, repeatDelay: 1});
     });
     secondPhone.to(".two .success-screen", {
         y: "-100%",
-        ease: Power2.easeOut,
-        duration: .7,
+        ease: Power3.easeOut,
+        duration: 1.2,
         delay: 1
     });
     secondPhone.to(".two .spinner", {
@@ -53,43 +190,30 @@ var secondPhone = gsap.timeline({repeat: -1, repeatDelay: 1});
         duration: 1.5
     });
     secondPhone.to(".two .spinner .top", {
-        borderLeftColor: "white",
+        borderRightColor: "white",
         duration: 0,
-        delay: -0.2
-    });
-    secondPhone.to(".two .spinner .right", {
-        rotate: 270,
-        duration: 1.5,
-        delay: -1.5
-    });
-    secondPhone.to(".two .spinner .right", {
-        borderLeftColor: "white",
-        duration: 0,
-        delay: -0.2
     });
     secondPhone.to(".two .spinner .bottom", {
         rotate: 180,
         duration: 1.5,
         delay: -1.5
     });
-    secondPhone.to(".two .spinner .bottom", {
-        borderLeftColor: "white",
+    secondPhone.to(".two .spinner .top", {
+        borderBottomColor: "white",
         duration: 0,
-        delay: -0.2
+        delay: -0.5
     });
-    secondPhone.to(".two .spinner .left", {
-        rotate: 90,
-        duration: 1.5,
-        delay: -1.5
+    secondPhone.to(".two .spinner", {
+        scale: 0.7,
+        duration: 0.2
     });
-    secondPhone.to(".two .spinner .left", {
-        borderLeftColor: "white",
-        duration: 0,
-        delay: -0.2
+    secondPhone.to(".two .spinner", {
+        scale: 1,
+        duration: 0.3
     });
     secondPhone.to(checkmark, {
         opacity: 1,
-        duration: 0,
+        duration: 0
     });
     secondPhone.to(checkmark, {
         display: "block",
@@ -105,7 +229,7 @@ var secondPhone = gsap.timeline({repeat: -1, repeatDelay: 1});
     secondPhone.to(checkmark, {
         height: "20px",
         width: "10px",
-        duration: 0.5,
+        duration: 0.5
     });
     //LOADING ANIMATION END
 
@@ -148,7 +272,7 @@ var thirdPhone = gsap.timeline({repeat: -1});
         height: "45px",
         x: 0,
         y: 0,
-        ease: Power2.easeOut,
+        ease: Power3.easeOut,
         duration: .3,
         delay: .2
     });
@@ -169,7 +293,7 @@ var thirdPhone = gsap.timeline({repeat: -1});
 
     //LOADING ANIMATION START
     thirdPhone.to(".three .spinner", {
-        display: "flex",
+        opacity: 1,
         duration: 0.2
     });
     thirdPhone.to(".three .spinner .top", {
@@ -177,40 +301,32 @@ var thirdPhone = gsap.timeline({repeat: -1});
         zIndex: 1,
         duration: 1.5
     });
+    thirdPhone.to(".three .spinner .top, .three .spinner .bottom", {
+        borderLeftColor: "black",
+        duration: 0,
+        delay: -1.3
+    });
     thirdPhone.to(".three .spinner .top", {
-        borderLeftColor: "black",
+        borderRightColor: "black",
         duration: 0,
-        delay: -0.2
-    });
-    thirdPhone.to(".three .spinner .right", {
-        rotate: 270,
-        duration: 1.5,
-        delay: -1.5
-    });
-    thirdPhone.to(".three .spinner .right", {
-        borderLeftColor: "black",
-        duration: 0,
-        delay: -0.2
     });
     thirdPhone.to(".three .spinner .bottom", {
         rotate: 180,
         duration: 1.5,
         delay: -1.5
     });
-    thirdPhone.to(".three .spinner .bottom", {
-        borderLeftColor: "black",
+    thirdPhone.to(".three .spinner .top", {
+        borderBottomColor: "black",
         duration: 0,
-        delay: -0.2
+        delay: -0.5
     });
-    thirdPhone.to(".three .spinner .left", {
-        rotate: 90,
-        duration: 1.5,
-        delay: -1.5
+    thirdPhone.to(".three .spinner", {
+        scale: 0.7,
+        duration: 0.2
     });
-    thirdPhone.to(".three .spinner .left", {
-        borderLeftColor: "black",
-        duration: 0,
-        delay: -0.2
+    thirdPhone.to(".three .spinner", {
+        scale: 1,
+        duration: 0.3
     });
     thirdPhone.to(checkmarkThirdPhone, {
         opacity: 1,
